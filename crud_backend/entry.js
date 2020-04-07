6dffeca933bd282d1c3203cca71f404546ca2ff2
KeenDawg <36858319+KeenDawg@users.noreply.github.com>
2020-04-07 01:51:26 +0100

    mongoose.conenct uri is set to atlas mongodb database

--- crud_backend/entry.js
+++ crud_backend/entry.js
@@ -3,14 +3,15 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyparser = require("body-parser");
 var cors = require("cors");
+var path = require('path');
 
 var app = express();
 
 const route = require('./route/routes');
 
 //Connecting to mongodb
-mongoose.connect('mongodb://localhost:27017/shoppinglist' ,{
-  useMongoClient: true
+mongoose.connect('mongodb+srv://testuser123:testuser123@cluster0-j6gtz.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser: true
 });
 
 //on connection
@@ -22,7 +23,7 @@ mongoose.connection.on('error', (err)=>{
   console.log(err);
 });
 
-const PORT = 3000
+const PORT = process.env.PORT || 8080;
 
 //app adding middleware - cors
 app.use(cors());
@@ -31,11 +32,17 @@ app.use(cors());
 app.use(bodyparser.json());
 
 app.use('/api',route);
-//
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
 
 app.get('/', (req,res)=>{
   res.send('foobar');
-})
+});
 
 app.listen(PORT, ()=>{
   console.log("Server has been started at port: " +PORT);
